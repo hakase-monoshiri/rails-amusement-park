beacb26152214bfd7d7a758d2656e513f30a9a7f
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-05-24 14:07:48 -0400

    commit tree 27f38120e3b44f9d8e44864723960dcaf388e549

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,22 @@
 class Ride < ActiveRecord::Base
+
+    belongs_to :attraction
+    belongs_to :user
+
+    def take_ride
+        if user.height >= attraction.min_height && user.tickets >= attraction.tickets
+            user.tickets -= attraction.tickets
+            user.nausea += attraction.nausea_rating
+            user.happiness += attraction.happiness_rating
+        elsif user.height < attraction.min_height && user.tickets < attraction.tickets
+            "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+        elsif user.height < attraction.min_height 
+            "Sorry. You are not tall enough to ride the #{attraction.name}."
+        elsif user.tickets < attraction.tickets
+            "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+        else
+            "You can't ride, for some unknown reason"
+        end
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+
+    has_many :rides
+    has_many :attractions, through: :rides
+
+    has_secure_password
+
+    def mood
+        if happiness >= nausea
+            "happy"
+        elsif nausea > happiness
+            "sad"
+        end
+    end
+
+
 end
